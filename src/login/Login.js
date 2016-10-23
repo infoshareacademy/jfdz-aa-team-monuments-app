2ee816c8310074857ddada875d5b04d7e365b584
kowalewsky <kowalewski.luki@gmail.com>
2016-10-23 12:54:01 +0200

    dodanie komponentu Login/Logout

--- src/login/Login.js
+++ src/login/Login.js
@@ -6,17 +6,27 @@ export default class Login extends React.Component {
     constructor(props) {
         super(props);
 
+        this.onClickChangeButtonText = this.onClickChangeButtonText.bind(this);
+
         this.state = {
            buttonText: "login"
         }
     }
+
+    onClickChangeButtonText() {
+        this.state.buttonText === "login"
+        ? this.setState({buttonText:"logout"})
+        : this.setState({buttonText:"login"})
+
+    }
+
 
     render() {
         return(
             <div className="Login-container">
                 <h3>Login</h3>
                 <p>Welcome</p>
-                <Button text={this.state.buttonText}>{this.state.buttonText}</Button>
+                <Button onClick={this.onClickChangeButtonText}>{this.state.buttonText}</Button>
             </div>
         )
     }
