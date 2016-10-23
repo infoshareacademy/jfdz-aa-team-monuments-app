71388955fc7058fb72819fa75df5aac6393c1e35
kowalewsky <kowalewski.luki@gmail.com>
2016-10-23 12:09:49 +0200

    dodanie biblioteki redux

--- src/index.js
+++ src/index.js
@@ -7,12 +7,17 @@ import MonumentsList from './monuments-list/MonumentsList'
 
 import './index.css';
 
+import { Provider } from 'react-redux'
+import store from './store'
+
 const root = document.getElementById('root');
 
 ReactDOM.render(
-    <Router history={hashHistory}>
-        <Route path="/" component={App} >
-            <Route path="show-list" component={MonumentsList}> </Route>
-        </Route>
-    </Router>,
-root);
+    <Provider store={store}>
+        <Router history={hashHistory}>
+         <Route path="/" component={App} >
+             <Route path="show-list" component={MonumentsList}> </Route>
+         </Route>
+        </Router>
+    </Provider>,root
+);
