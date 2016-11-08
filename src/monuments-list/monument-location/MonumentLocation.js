daa9fc36c9ac711f38fcb81b3c0eedee6d885562
Zaxaar <zahar1982@gmail.com>
2016-11-08 14:45:07 +0100

    zagnieżdżenie monument-location w details

--- src/monuments-list/monument-location/MonumentLocation.js
+++ src/monuments-list/monument-location/MonumentLocation.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { connect}  from 'react-redux'
-import { displayMonumentLocation , hideMonumentLocation } from './actionCreators'
+import { displayMonumentLocation } from './actionCreators'
 import { Button , Glyphicon } from 'react-bootstrap'
 import './MonumentLocation.css'
 
@@ -10,24 +10,17 @@ const mapStateToProps = (state) => ({
 })
 
 const mapDispatchToProps = (dispatch) => ({
-    displayMonumentLocation: (lat, lon) => dispatch (displayMonumentLocation(lat,lon) ) ,
-    hideMonumentLocation: () => dispatch(hideMonumentLocation())
+    displayMonumentLocation: (lat, lon) => dispatch (displayMonumentLocation(lat,lon) ) 
 })
 
 const MonumentLocation = ({
     currentMonumentLat,
     currentMonumentLon,
-    displayMonumentLocation , 
-    hideMonumentLocation 
+    displayMonumentLocation ,
 }) => (
-    <div className="monument-location-column">
         <Button className="monument-location-button" bsStyle ="warning" onClick={() => displayMonumentLocation(currentMonumentLat , currentMonumentLon) } >
             <Glyphicon glyph="map-marker" />
         </Button>
-        <Button className="monument-location-button" bsStyle ="danger" onClick={() => hideMonumentLocation() }>
-            <Glyphicon glyph="trash" />
-        </Button>
-    </div>
 )
 
 export default connect (mapStateToProps , mapDispatchToProps)(MonumentLocation)
