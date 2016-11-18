59bbe3e2b019b20ab959092a6ac864fccf1517d5
molszewska <ma.olszewska@op.pl>
2016-11-18 14:53:32 +0100

    showing content for logged in user only and redirect after log out

--- src/App.js
+++ src/App.js
@@ -1,20 +1,59 @@
 import React from 'react'
 import {Link} from 'react-router'
 import {Button} from 'react-bootstrap'
+import { connect } from 'react-redux'
+import { logOut } from './login/actionCreators';
+import { browserHistory } from 'react-router'
+
 import './App.css';
 
 import Footer from './footer/Footer'
 
+const mapStateToProps = (state) => ({
+  loggedIn: state.login.loggedIn,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  logOut: () => dispatch(logOut())
+});
+
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.logOut = this.logOut.bind(this);
+  }
+
+  logOut() {
+    this.props.logOut();
+    browserHistory.push('/')
+  }
+
+  renderLoggedInLinks() {
+    if (!this.props.loggedIn) {
+      return null;
+    }
+
+    return (<span>
+      <Link to='map' ><Button> Mapa </Button></Link>
+      <Link to='list' ><Button> Lista </Button></Link>
+      <Link to='user-monuments' ><Button> Moje </Button></Link>
+    </span>);
+  }
+
+  renderLoginButton() {
+    if (this.props.loggedIn) {
+      return <Button onClick={this.logOut.bind(this)}>Log out</Button>;
+    }
+    return <Link to='login' ><Button> Login </Button></Link>;
+  }
+
   render() {
     return (
       < div className="App">
         <div className="App-header">
           <Link to='/' ><Button> #Home </Button></Link>
-          <Link to='map' ><Button> Mapa </Button></Link>
-          <Link to='list' ><Button> Lista </Button></Link>
-          <Link to='user-monuments' ><Button> Moje </Button></Link>
-          <Link to='login' ><Button> Login </Button></Link>
+          {this.renderLoggedInLinks()}
+          {this.renderLoginButton()}
         </div>
 
           {this.props.children}
@@ -25,4 +64,4 @@ class App extends React.Component {
   }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App)
