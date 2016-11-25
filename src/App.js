34fa858c2ebe51dae349609f82aa87e6b0fde049
Zaxaar <zahar1982@gmail.com>
2016-11-25 17:26:06 +0100

    ...

--- src/App.js
+++ src/App.js
@@ -1,64 +1,75 @@
 import React from 'react'
 import {Link} from 'react-router'
-import {Button} from 'react-bootstrap'
+import {Navbar, Nav, NavItem, Button} from 'react-bootstrap'
 import { connect } from 'react-redux'
-import { logOut } from './login/actionCreators';
+import { logOut } from './login/actionCreators'
 import { browserHistory } from 'react-router'
+import logo from './logo.svg'
+import './App.css'
 
-import './App.css';
-
+import Login from './login/Login'
 import Footer from './footer/Footer'
 
 const mapStateToProps = (state) => ({
-  loggedIn: state.login.loggedIn,
+    loggedIn: state.login.loggedIn,
+    userName: state.login.userData.firstName
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  logOut: () => dispatch(logOut())
+    logOut: () => dispatch(logOut())
 });
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.logOut = this.logOut.bind(this);
+    constructor(props) {
+        super(props);
+        this.logOut = this.logOut.bind(this);
   }
-
+    
   logOut() {
-    this.props.logOut();
-    browserHistory.push('/')
+      this.props.logOut();
+      browserHistory.push('/')
   }
 
   renderLoggedInLinks() {
     if (!this.props.loggedIn) {
       return null;
     }
-
-    return (<span>
-      <Link to='map' ><Button> Mapa </Button></Link>
-      <Link to='list' ><Button> Lista </Button></Link>
-      <Link to='user-monuments' ><Button> Moje </Button></Link>
-    </span>);
+      return (
+        <Nav className="nav-elements">
+            <NavItem><Link className="nav-element" to='list' > Znajdź zabytek </Link></NavItem>
+            <NavItem><Link className="nav-element" to='user-monuments'> Moje zabytki </Link></NavItem>
+        </Nav> )
   }
 
   renderLoginButton() {
     if (this.props.loggedIn) {
-      return <Button onClick={this.logOut.bind(this)}>Log out</Button>;
+      return <Button bsStyle="danger" className="login-button" onClick={this.logOut.bind(this)}>{this.props.userName} | Log out</Button>;
     }
-    return <Link to='login' ><Button> Login </Button></Link>;
+    return <Login />;
   }
 
   render() {
-    return (
-      < div className="App">
-        <div className="App-header">
-          <Link to='/' ><Button> #Home </Button></Link>
-          {this.renderLoggedInLinks()}
-          {this.renderLoginButton()}
-        </div>
+      return (
+          <div className="App">
+              <Navbar  id="App-navigation" inverse>
+                  <Navbar.Header >
+                      <Link to='/' >
+                          <img src={logo} className="App-logo" alt="logo" />
+                      </Link>
+                      <Navbar.Toggle />
+                  </Navbar.Header>
 
-          {this.props.children}
-          <Footer />
+                  <Navbar.Collapse>
+                      {this.renderLoggedInLinks()}
+                      <Nav className="login-button" pullRight>
+                          <NavItem>{this.renderLoginButton()}</NavItem>
+                      </Nav>
 
+                  </Navbar.Collapse>
+              </Navbar>
+
+              {this.props.children}
+        <Footer />
       </div>
     );
   }
--- src/login/Login.js
+++ src/login/Login.js
@@ -2,7 +2,9 @@ import React from 'react'
 import { connect } from 'react-redux' 
 import './Login.css'
 import GoogleLogin from 'react-google-login'
-import { loginSuccess, loginFailure, logOut, persistUserData } from './actionCreators';
+import { loginSuccess, loginFailure, logOut, persistUserData } from './actionCreators'
+
+import {Glyphicon} from 'react-bootstrap'
 
 const mapStateToProps = (state) => ({
     userData: state.login.userData,
@@ -19,14 +21,14 @@ const mapDispatchToProps = (dispatch) => ({
 })
 
 const Login = ({ userData, loggedIn, loginSuccess }) => (
-     <div>
-         <h1>Login</h1>
               <GoogleLogin
-                   clientId="125511495520-i1vk2u8c4f9nk6bmtb5i8evculbqfvur.apps.googleusercontent.com"
-                   buttonText="Log in"
-                   onSuccess={(x) => loginSuccess(x)}
-                   onFailure={(x) => alert(x)}
-              />
-     </div>
+                  className="btn btn-success google-login-button"
+                  clientId="125511495520-i1vk2u8c4f9nk6bmtb5i8evculbqfvur.apps.googleusercontent.com"
+                  onSuccess={(x) => loginSuccess(x)}
+                  onFailure={(x) => alert(x)}>
+                  <Glyphicon glyph="user" />
+                  <span className="login-btn-text">Zaloguj się - Google</span>
+              </GoogleLogin>
 )
+
 export default connect(mapStateToProps,mapDispatchToProps)(Login)
